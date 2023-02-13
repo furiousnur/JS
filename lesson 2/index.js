778d8afa9a9c15830db588b0d56b371684dbabe0
Nur <furiousnur@gmail.com>
2023-02-14 01:29:59 +0600

    js lesson 1 and 2

---
+++ lesson 2/index.js
@@ -0,0 +1,19 @@
+//you can run this program by using the command =>   node program4 .js or adding this file in html
+
+// string  -> a sequnces of characters
+// string concatenation -> adding multiple strings
+
+var fName = "Nur";
+var lName = "Alam";
+
+// adding multiple strings here
+var fullName = fName + " " + lName;
+document.write(fullName);
+console.log(fullName);
+console.log("Today is" + " a " + "beautiful day");
+console.log("My name is " + fullName);
+
+var num1 = 20;
+var num2 = 30;
+var sum = num1 + num2;
+console.log(num1 + " + " + num2 + " = " + sum);
